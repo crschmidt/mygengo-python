9319a106f2249b31b150555017456be70cb39112
Ryan McGrath <ryan@venodesigns.net>
2011-01-18 04:11:34 -0500

    Almost launch ready, just need to do some Array flattening...

--- pygengo/mockdb.py
+++ pygengo/mockdb.py
@@ -3,21 +3,106 @@
 
 	Parameters that need to be embedded in the URL are treated with mustaches, e.g:
 
-	{{version}}, etc
+	{{bert}}, etc
 
 	When creating new endpoint definitions, keep in mind that the name of the mustache
 	will be replaced with the keyword that gets passed in to the function at call time.
 
-	i.e, in this case, if I pass version = 47 to any function, {{version}} will be replaced
+	i.e, in this case, if I pass bert = 47 to any function, {{bert}} will be replaced
 	with 47, instead of defaulting to 1 (said defaulting takes place at conversion time).
 """
 
-# Base PyGengo API url, no need to repeat this junk...
-base_url = 'http://api.twitter.com/{{version}}'
+# myGengo API urls. {{version}} gets replaced with v1/etc
+# at run time.
+api_urls = {
+	'base': 'http://api.mygengo.com/{{version}}',
+	'sandbox': 'http://api.sandbox.mygengo.com/{{version}}',
+}
 
+# The API endpoint 'table', 'database', 'hash', 'dictionary', whatever
+# you'd like to call it. To keep things uber nice and organized, we secure away
+# all the endpoints here with easily replaceable scenarios. Win!
 apihash  = {
-	'getRateLimitStatus': {
-		'url': '/account/rate_limit_status.json',
+	# All Account-information based methods...
+	'getAccountStats': {
+		'url': '/account/stats',
+		'method': 'GET',
+	},
+	'getAccountBalance': {
+		'url': '/account/balance',
+		'method': 'GET',
+	},
+
+	# Creating new translation requests.
+	'postTranslationJob': {
+		'url': '/translate/job',
+		'method': 'POST',
+	},
+	'postTranslationJobs': {
+		'url': '/translate/jobs',
+		'method': 'POST',
+	},
+
+	# Updating an existing translation request.
+	'updateTranslationJob': {
+		'url': '/translate/job/{{id}}',
+		'method': 'PUT',
+	},
+
+	# Viewing existing translation requests.
+	'getTranslationJob': {
+		'url': '/translate/job/{{id}}',
+		'method': 'GET',
+	},
+	'getTranslationJobs': {
+		'url': '/translate/jobs',
+		'method': 'GET',
+	},
+	'getTranslationJobBatch': {
+		'url': '/translate/jobs/{{id}}',
+		'method': 'GET',
+	},
+
+	# Deal with comments and other metadata about a TranslationJob in progress.
+	'postTranslationJobComment': {
+		'url': '/translate/job/{{id}}/comment',
+		'method': 'POST',
+	},
+	'getTranslationJobComments': {
+		'url': '/translate/job/{{id}}/comments',
+		'method': 'GET',
+	},
+	'getTranslationJobFeedback': {
+		'url': '/translate/job/{{id}}/feedback',
+		'method': 'GET',
+	},
+	'getTranslationJobRevisions': {
+		'url': '/translate/job/{{id}}/revisions',
+		'method': 'GET',
+	},
+	'getTranslationJobRevision': {
+		'url': '/translate/job/{{id}}/revisions/{{revision_id}}',
+		'method': 'GET',
+	},
+	'getTranslationJobPreviewImage': {
+		'url': '/translate/job/{{id}}/preview',
+		'method': 'GET',
+	},
+
+	# Delete a job...
+	'deleteTranslationJob': {
+		'url': '/translate/job/{{id}}',
+		'method': 'DELETE',
+	},
+
+	# Translation Service language information. Holds information
+	# about which languages can be converted to which, etc.
+	'getServiceLanguagePairs': {
+		'url': '/translate/service/language_pairs',
+		'method': 'GET',
+	},
+	'getServiceLanguages': {
+		'url': '/translate/service/languages',
 		'method': 'GET',
 	},
 }
--- pygengo/pygengo.py
+++ pygengo/pygengo.py
@@ -11,38 +11,40 @@
 __author__ = 'Ryan McGrath <ryan@venodesigns.net>'
 __version__ = '1.0.0'
 
-import urllib
-import urllib2 import urlopen, HTTPError
-import urlparse
-import httplib
 import httplib2
 import mimetypes
 import mimetools
 import re
+import hmac
 
-# Twython maps keyword based arguments to Twitter API endpoints. The endpoints
-# table is a file with a dictionary of every API endpoint that Twython supports.
-from pygengo_mockdb import base_url, apihash
+from hashlib import sha1
+from urllib import urlencode
+from time import time
+from operator import itemgetter
+
+# pygengo_mockdb is a file with a dictionary of every API endpoint for myGengo.
+from mockdb import api_urls, apihash
+from error_codes import errors
 
 # There are some special setups (like, oh, a Django application) where
 # simplejson exists behind the scenes anyway. Past Python 2.6, this should
 # never really cause any problems to begin with.
 try:
 	# Python 2.6 and up
-	import json as simplejson
+	import json
 except ImportError:
 	try:
 		# Python 2.6 and below (2.4/2.5, 2.3 is not guranteed to work with this library to begin with)
-		import simplejson
+		import simplejson as json
 	except ImportError:
 		try:
 			# This case gets rarer by the day, but if we need to, we can pull it from Django provided it's there.
-			from django.utils import simplejson
+			from django.utils import simplejson as json
 		except:
 			# Seriously wtf is wrong with you if you get this Exception.
-			raise Exception("PyGengo requires the simplejson library (or Python 2.6) to work. http://www.undefined.org/python/")
+			raise Exception("PyGengo requires the simplejson library (or Python 2.6+) to work. http://www.undefined.org/python/")
 
-class TwythonError(Exception):
+class PyGengoError(Exception):
 	"""
 		Generic error class, catch-all for most PyGengo issues.
 		Special cases are handled by APILimit and AuthError.
@@ -53,27 +55,16 @@ class TwythonError(Exception):
 	"""
 	def __init__(self, msg, error_code=None):
 		self.msg = msg
-		if error_code == 400:
-			raise APILimit(msg)
+		if error_code == 1000:
+			# Auth errors tend to be the most requested for their own
+			# Exception instances, so give it to the masses, yo.
+			raise PyGengoAuthError(msg)
 	
 	def __str__(self):
 		return repr(self.msg)
 
 
-class APILimitError(PyGengoError):
-	"""
-		Raised when you've hit an API limit. Try to avoid these, read the API
-		docs if you're running into issues here, PyGengo does not concern itself with
-		this matter beyond telling you that you've done goofed.
-	"""
-	def __init__(self, msg):
-		self.msg = msg
-	
-	def __str__(self):
-		return repr(self.msg)
-
-
-class AuthError(TwythonError):
+class PyGengoAuthError(PyGengoError):
 	"""
 		Raised when you try to access a protected resource and it fails due to some issue with
 		your authentication.
@@ -86,24 +77,33 @@ class AuthError(TwythonError):
 
 
 class PyGengo(object):
-	def __init__(self, headers = None):
-		"""setup(self, headers = None)
+	def __init__(self, public_key = None, private_key = None, sandbox = False, api_version = 1, headers = None):
+		"""PyGengo(public_key = None, private_key = None, sandbox = False, headers = None)
 
 			Instantiates an instance of PyGengo. While you can have multiple instances of PyGengo going at once, this is not recommended
 			practice, and this library will never be held responsible for anything myGengo may do to you if you decide to go down this route.
 			
 			Parameters:
+				public_key - Your 'public' key for myGengo. Retrieve this from your account information if you want to do authenticated calls.
+				private_key - Your 'private' key for myGengo. Retrieve this from your account information if you want to do authenticated calls.
+				sandbox - Whether to use the myGengo sandbox or not. Check with myGengo for the differences with this as it may change.
 				headers - User agent header, dictionary style ala {'User-Agent': 'Bert'}
 		"""
+		self.api_url = api_urls['sandbox'] if sandbox is True else api_urls['base']
+		self.api_version = api_version
+		self.public_key = public_key
+		self.private_key = private_key
 		# If there's headers, set them, otherwise be an embarassing parent for their own good.
 		self.headers = headers
 		if self.headers is None:
-			headers = {'User-agent': 'PyGengo Python myGengo Library v%s' % __version__}
+			self.headers = {'User-agent': 'PyGengo Python myGengo Library v%s' % __version__}
+		# No matter whether we get some supplied or use the generic, tell it we want JSON. ;P
+		self.headers['Accept'] = 'application/json'
 		self.client = httplib2.Http()
 	
 	def __getattr__(self, api_call):
 		"""
-			The most magically awesome block of code you'll see in 2010.
+			The most magically awesome block of code you'll ever see.
 
 			Rather than list out 9 million damn methods for this API, we just keep a table (see above) of
 			every API endpoint and their corresponding function id for this library. This pretty much gives
@@ -113,29 +113,57 @@ class PyGengo(object):
 			For those who don't get what's going on here, Python classes have this great feature known as __getattr__().
 			It's called when an attribute that was called on an object doesn't seem to exist - since it doesn't exist,
 			we can take over and find the API method in our table. We then return a function that downloads and parses
-			what we're looking for, based on the keywords passed in.
+			what we're looking for, based on the key/values passed in.
 
 			I'll hate myself for saying this, but this is heavily inspired by Ruby's "method_missing".
+
+			Note: I'm largely borrowing this technique from another API library/wrapper I've written in the past (Twython).
+			If you happen to read both sources and find the same text... well, that's why. ;)
 		"""
 		def get(self, **kwargs):
-			# Go through and replace any mustaches that are in our API url.
-			fn = api_table[api_call]
+			# Grab the (hopefully) existing method 'definition' to fire off from our api hash table.
+			fn = apihash[api_call]
+			
+			# Set up a true base URL, abstracting away the need to care about the sandbox mode
+			# or API versioning at this stage.
+			base_url = self.api_url.replace('{{version}}', 'v%d' % self.api_version)
+			
+			# Go through and replace any mustaches that are in our API url with their appropriate key/value pairs...
 			base = re.sub(
 				'\{\{(?P<m>[a-zA-Z]+)\}\}',
-				lambda m: "%s" % kwargs.get(m.group(1), '1'), # The '1' here catches the API version. Slightly hilarious.
+				lambda m: "%s" % kwargs.get(m.group(1), '1'), # I'll just leave this here...
 				base_url + fn['url']
 			)
-
+			
+			# Build up a proper 'authenticated' url...
+			#
+			# Note: for further information on what's going on here, it's best to familiarize yourself
+			# with the myGengo authentication API.
+			query_params = dict([k, v.encode('utf-8')] for k, v in kwargs.items())
+			query_params['api_key'] = self.public_key
+			query_params['ts'] = int(time())
+			query_string = urlencode(sorted(query_params.items(), key = itemgetter(0)))
+			query_hmac = hmac.new(self.private_key, query_string, sha1)
+			query_params['api_sig'] = query_hmac.hexdigest()
+			query_string = urlencode(query_params)
+			
 			# Then open and load that shiiit, yo. TODO: check HTTP method and junk, handle errors/authentication
-			if fn['method'] == 'POST':
-				resp, content = self.client.request(base, fn['method'], urllib.urlencode(dict([k, v.encode('utf-8')] for k, v in kwargs.items())))
+			if fn['method'] == 'POST' or fn['method'] == 'PUT':
+				resp, content = self.client.request(base, fn['method'], query_string, headers = self.headers)
 			else:
-				url = base + "?" + "&".join(["%s=%s" %(key, value) for (key, value) in kwargs.iteritems()])
-				resp, content = self.client.request(url, fn['method'])
-
-			return simplejson.loads(content)
-
-		if api_call in api_table:
+				resp, content = self.client.request(base + '?%s' % query_string, fn['method'], headers = self.headers)
+			
+			# Load this into native Python...
+			results = json.loads(content)
+			
+			# See if we got any weird or odd errors back that we can cleanly raise on or something...
+			if 'optstat' in results:
+				raise PyGengoError(errors[`results['err']['code']`], results['err']['code'])
+			
+			# If not, screw it, return the junks!
+			return json.loads(content)
+			
+		if api_call in apihash:
 			return get.__get__(self)
 		else:
 			raise AttributeError, api_call
